0b759bf4cb878a5bd336a9270bc421d7ca7c2518
Eric NG <mr5iff@gmail.com>
2019-03-24 12:08:24 +0800

    flake8

--- funding_monitor/run_all_spiders.py
+++ funding_monitor/run_all_spiders.py
@@ -5,7 +5,7 @@ setting = get_project_settings()
 process = CrawlerProcess(setting)
 
 for spider_name in process.spider_loader.list():
-    print ("Running spider %s" % (spider_name))
+    print("Running spider %s" % (spider_name))
     process.crawl(spider_name)
 
 process.start()  # the script will block here until all crawling jobs are finished
